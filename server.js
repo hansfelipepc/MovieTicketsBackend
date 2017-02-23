ac9489fe6b6e6a3b97370b7f25aa9e1514e333af
Hans Felipe <hansfelipepc@outlook.com>
2017-02-23 00:55:38 -0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,14 @@
+import express from 'express';
+
+// Initialize http server
+const app = express();
+
+// Handle / route
+app.get('/', (req, res) =>
+res.send('Hello World!')
+);
+
+const server = app.listen(3000, () => {
+    const { address, port } = server.address();
+console.log(`Listening at http://${address}:${port}`);
+});
